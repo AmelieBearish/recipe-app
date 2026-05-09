776d52275a4d9aba5f68a484c5eb0daf3dc10ab4
AmelieBearish <dexshien@gmail.com>
2026-05-09 09:43:30 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -105,7 +105,7 @@ export default function Home() {
         {filteredRecipes.map(recipe => (
           <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
-      </div>
-    </div>
-  )
-}
+            </div>
+          </div>
+        </div>
+      )}
